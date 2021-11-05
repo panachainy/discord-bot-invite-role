7b9577eacecc2e378010283dffec5b8091df9a24
Panachai <panachai.ny@gmail.com>
2021-11-05 13:39:33 +0700

    clean bot

--- src/bot.js
+++ src/bot.js
@@ -1,47 +1,26 @@
 const { Client, Intents } = require("discord.js");
 
-function init(BOT_TOKEN) {
-  const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+require("dotenv").config();
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
-  client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
-    //   const util = require("util");
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-    //   console.log(
-    //     util.inspect(client, {
-    //       showHidden: false,
-    //       depth: null,
-    //       colors: true,
-    //     })
-    //   );
-  });
+client.on("disconnect", function (msg, code) {
+  if (code === 0) return console.error(msg);
+  bot.connect();
+});
 
-  client.on("message", async (message) => {
-    console.log(`1sssssssssssssssssss ${message}`);
-  });
+client.on("messageCreate", async (msg) => {
+  if (msg.content === "ping") {
+    console.log("ping");
+    msg.reply("pong");
+  }
+});
 
-  client.on("messageCreate", async (message) => {
-    console.log(`2sssssssssssssssssss ${message}`);
-  });
-
-  client.on("interactionCreate", async (interaction) => {
-    console.log("123123");
-
-    if (!interaction.isCommand()) return;
-
-    if (interaction.commandName === "ping") {
-      await interaction.reply("Pong!");
-    }
-  });
-
-  client.on("messageReaction", (messageReaction, user) => {
-    console.log(
-      `========== messageReaction: ${messageReaction} / user: ${user}`
-    );
-  });
-
-  client.login(BOT_TOKEN);
-}
-
-exports.init = init;
+client.login(BOT_TOKEN);

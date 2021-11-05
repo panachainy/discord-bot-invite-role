25ec279957a7503df398776f4333cdfbccc77141
Panachai <panachai.ny@gmail.com>
2021-11-05 14:36:26 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,26 +1,67 @@
-const { Client, Intents } = require("discord.js");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 
 require("dotenv").config();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+// const util = require("util");
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+function dmProcess(message) {
+  console.log("this is DM ===========", Message);
+  message.reply("pong");
+}
 
-client.on("disconnect", function (msg, code) {
-  if (code === 0) return console.error(msg);
-  bot.connect();
-});
+function Init() {
+  const client = new Client({
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.DIRECT_MESSAGES,
+    ],
+  });
 
-client.on("messageCreate", async (msg) => {
-  if (msg.content === "ping") {
-    console.log("ping");
-    msg.reply("pong");
-  }
-});
+  client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
-client.login(BOT_TOKEN);
+  client.on("disconnect", function (msg, code) {
+    if (code === 0) return console.error(msg);
+    bot.connect();
+  });
+
+  client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+
+    if (message.channel.type === "DM") {
+      dmProcess(message);
+      return;
+    }
+
+    message.author.createDM().then((dm) => {
+      dm.send(`สวัสดีคุณ ${message.author.username}
+      รบกวนป้อนเบอร์โทรศัพท์ เพื่อทำการลงทะเบียน Moomall`);
+      console.log("dm done");
+    });
+
+    if (message.content === "ping") {
+      console.log("ping");
+      message.reply("pong");
+    }
+
+    // const dmLogEmbed = new MessageEmbed()
+    //   .setColor("RANDOM")
+    //   .setTitle(`${message.author.tag} dmed the bot and said: `)
+    //   .setDescription(message.content)
+    //   .setFooter(`User's id: ${message.author.id}`);
+    // dmLogEmbed.setImage(
+    //   "https://stg-salepage.moomall.com/api/moomall/images/logo.png"
+    // );
+
+    // message.reply(dmLogEmbed);
+  });
+
+  client.on("");
+
+  client.login(BOT_TOKEN);
+}
+
+Init();
